60c50128cd5d77e2cb7ab3ea0a20b7646fcb9249
Dev <Dev@DEV-LAPTOP>
2017-06-13 21:59:21 +0200

    Updated Comic Books List view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -16,6 +16,14 @@ namespace ComicBookGallery.Controllers
         {
             _comicBookRepository = new Data.ComicBookRepository();
         }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetCommicBooks();
+
+            return View(comicBooks);
+        }
+
         public ActionResult Detail(int? id)
         {
             if (id == null)
